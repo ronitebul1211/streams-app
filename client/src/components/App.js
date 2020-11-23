801d4aaecb4390b75e074b3549a1bd5caf548dfa
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-23 20:41:12 +0200

    create base components structure and routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,25 +1,21 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
-const PageOne = () => {
-   return (
-      <div>
-         Page One
-         <Link to="/page-two">Navigate to page two</Link>
-      </div>
-   );
-};
-
-const PageTwo = () => {
-   return <div>PageTwo</div>;
-};
+import StreamList from "./streams/StreamList";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
    return (
       <div>
          <BrowserRouter>
-            <Route path="/" exact component={PageOne} />
-            <Route path="/page-two" component={PageTwo} />
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit" exact component={StreamEdit} />
+            <Route path="/streams/delete" exact component={StreamDelete} />
+            <Route path="/streams/show" exact component={StreamShow} />
          </BrowserRouter>
       </div>
    );
