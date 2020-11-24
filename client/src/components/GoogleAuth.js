20cc9b0ef717cff90c7089dd77bfb3e833f93a9d
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-24 09:37:11 +0200

    implement ui to indicate the user if he sign in or sign out

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -18,17 +18,37 @@ class GoogleAuth extends Component {
             .then(() => {
                this.auth = window.gapi.auth2.getAuthInstance();
                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+               this.auth.isSignedIn.listen(this.onAuthChange);
             });
       });
    }
 
+   onAuthChange = () => {
+      this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+   };
+
+   onSignIn = () => {
+      this.auth.signIn();
+   };
+   onSignOut = () => {
+      this.auth.signOut();
+   };
+
    renderAuthButton() {
       if (this.state.isSignedIn === null) {
-         return <div>don't know</div>;
+         return null;
       } else if (this.state.isSignedIn) {
-         return <div>signed in</div>;
+         return (
+            <button className="ui red google button" onClick={() => this.onSignOut()}>
+               <i className="google icon">Sign Out</i>
+            </button>
+         );
       } else {
-         return <div>signed out</div>;
+         return (
+            <button className="ui red google button" onClick={() => this.onSignIn()}>
+               <i className="google icon">Sign in with google</i>
+            </button>
+         );
       }
    }
    render() {
